b00031847ea54f1aa768ca23fbc413df8692b097
Lauri Kotilainen <lauri@offbeat.fi>
2017-07-19 20:30:54 +0300

    - Output rulesets and media blocks in the order that they appear

--- LessonNet.Parser/ParseTree/Stylesheet.cs
+++ LessonNet.Parser/ParseTree/Stylesheet.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using LessonNet.Parser.CodeGeneration;
+using LessonNet.Parser.ParseTree.Mixins;
 using LessonNet.Parser.Util;
 
 namespace LessonNet.Parser.ParseTree
@@ -15,37 +16,33 @@ namespace LessonNet.Parser.ParseTree
 		}
 
 		protected override IEnumerable<LessNode> EvaluateCore(EvaluationContext context) {
-			(var rulesets, var mediaBlocks) = EvaluateStatements(context).Split<Ruleset, MediaBlock>();
-
-			yield return new Stylesheet(rulesets.Concat<Statement>(mediaBlocks));
+			yield return new Stylesheet(EvaluateStatements(context));
 		}
 
 		private IEnumerable<Statement> EvaluateStatements(EvaluationContext context) {
 			// Handle variables and mixin definitions first: Variable scoping rules dictate that within a given
 			// variable scope, the last declaration is the one that takes effect for
 			// both the current scope and child scopes.
-			(var variables, var declarations, var mediaBlocks, var otherStatements) = Statements.Split<VariableDeclaration, Declaration, MediaBlock, Statement>();
+			(var variables, var mixinDefinitions, var otherStatements) = Statements.Split<VariableDeclaration, MixinDefinition, Statement>();
 			// Variables first, because declaring rulesets evaluates selectors that may depend on those variables
 			foreach (var variable in variables) {
 				variable.DeclareIn(context);
 			}
 
-			foreach (var declaration in declarations) {
+			foreach (var declaration in mixinDefinitions) {
 				declaration.DeclareIn(context);
 			}
 
-			// Rulesets are both declarations and general statements (invokable but also output-producing).
-			var rulesets = declarations.OfType<Ruleset>();
-
-			foreach (var mediaBlock in mediaBlocks) {
-				foreach (var resultMediaBlock in mediaBlock.Evaluate(context).Cast<MediaBlock>()) {
-					yield return resultMediaBlock;
-				}
+			// Rulesets are both declarations and general statements (invokable but also output-producing). Declare them before evaluating,
+			// because an invocation may appear before the declaration in the source.
+			var rulesets = otherStatements.OfType<Ruleset>();
+			foreach (var ruleset in rulesets) {
+				ruleset.DeclareIn(context);
 			}
 
-			foreach (var statement in rulesets.Concat(otherStatements)) {
-				foreach (var generatedNode in statement.Evaluate(context)) {
-					yield return (Statement) generatedNode;
+			foreach (var statement in otherStatements) {
+				foreach (var result in statement.Evaluate(context)) {
+					yield return (Statement) result;
 				}
 			}
 		}
