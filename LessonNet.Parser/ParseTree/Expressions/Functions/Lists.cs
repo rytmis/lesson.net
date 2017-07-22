eb523d36cc8427a109d08bfcea95908da442a4e3
Lauri Kotilainen <lauri@offbeat.fi>
2017-07-22 14:57:15 +0300

    - Simplified length function

--- LessonNet.Parser/ParseTree/Expressions/Functions/Lists.cs
+++ LessonNet.Parser/ParseTree/Expressions/Functions/Lists.cs
@@ -8,15 +8,6 @@ namespace LessonNet.Parser.ParseTree.Expressions.Functions {
 	public class LengthFunction : LessFunction {
 		public LengthFunction(Expression arguments) : base(arguments) { }
 
-		protected override IEnumerable<LessNode> EvaluateCore(EvaluationContext context) {
-			var variable = Arguments as Variable;
-			if (variable != null) {
-				yield return EvaluateFunction(variable.EvaluateSingle<Expression>(context));
-			} else {
-				yield return EvaluateFunction(Arguments.EvaluateSingle<Expression>(context));
-			}
-		}
-
 		protected override Expression EvaluateFunction(Expression arguments) {
 			if (arguments is ExpressionList list) {
 				return new Measurement(list.Values.Count, "");
