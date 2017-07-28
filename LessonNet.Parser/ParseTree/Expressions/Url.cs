0ade992445cda3cb724ec793f411e4cf4c503cb9
Lauri Kotilainen <lauri@offbeat.fi>
2017-07-28 13:34:42 +0300

    - Expose Url expression members

--- LessonNet.Parser/ParseTree/Expressions/Url.cs
+++ LessonNet.Parser/ParseTree/Expressions/Url.cs
@@ -3,27 +3,27 @@ using LessonNet.Parser.CodeGeneration;
 
 namespace LessonNet.Parser.ParseTree.Expressions {
 	public class Url : Expression {
-		private readonly LessString str;
-		private readonly Variable var;
-		private readonly string url;
+		public LessString StringContent { get; }
+		public Variable VariableContent { get; }
+		public string RawUrl { get; }
 
 		public Url(LessString str) {
-			this.str = str;
+			this.StringContent = str;
 		}
 
 		public Url(Variable var) {
-			this.var = var;
+			this.VariableContent = var;
 		}
 
 		public Url(string url) {
-			this.url = url;
+			this.RawUrl = url;
 		}
 
 		protected override IEnumerable<LessNode> EvaluateCore(EvaluationContext context) {
-			if (var != null) {
-				yield return new Url(var.EvaluateSingle<LessString>(context));
-			} else if (str != null) {
-				yield return new Url(str.EvaluateSingle<LessString>(context));
+			if (VariableContent != null) {
+				yield return new Url(VariableContent.EvaluateSingle<LessString>(context));
+			} else if (StringContent != null) {
+				yield return new Url(StringContent.EvaluateSingle<LessString>(context));
 			} else {
 				yield return this;
 			}
@@ -32,17 +32,17 @@ namespace LessonNet.Parser.ParseTree.Expressions {
 		public override void WriteOutput(OutputContext context) {
 			context.Append("url(");
 
-			if (str != null) {
-				context.Append(str);
-			} else if (url != null) {
-				context.Append(url);
+			if (StringContent != null) {
+				context.Append(StringContent);
+			} else if (RawUrl != null) {
+				context.Append(RawUrl);
 			}
 
 			context.Append(")");
 		}
 
 		protected bool Equals(Url other) {
-			return string.Equals(url, other.url);
+			return string.Equals(RawUrl, other.RawUrl);
 		}
 
 		public override bool Equals(object obj) {
@@ -53,7 +53,7 @@ namespace LessonNet.Parser.ParseTree.Expressions {
 		}
 
 		public override int GetHashCode() {
-			return 397 ^ (url != null ? url.GetHashCode() : 0);
+			return 397 ^ (RawUrl != null ? RawUrl.GetHashCode() : 0);
 		}
 	}
 }
